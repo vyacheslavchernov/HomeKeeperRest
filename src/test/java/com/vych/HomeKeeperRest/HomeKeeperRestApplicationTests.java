dac1cb67e9bb8ee9af0063f7947ac22854f3a9a2
Vyacheslav Chernov <rainboss1234@gmail.com>
2022-11-16 18:28:47 +0700

    init

---
+++ src/test/java/com/vych/HomeKeeperRest/HomeKeeperRestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vych.HomeKeeperRest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HomeKeeperRestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
